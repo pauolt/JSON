ca2f73585db50455692ebd531524a9460c4a8e61
pauolt2 <pauolt2@alu.edu.gva.es>
2025-03-12 09:04:27 +0100

    Acabado

--- JAVASCRIPT/ResumJS/script.js
+++ JAVASCRIPT/ResumJS/script.js
@@ -1,13 +1,12 @@
 let nom = prompt("Nom?");
 let edat = parseInt(prompt("Edat?"));
 
-let num_pel = parseInt(prompt("Quantes pel·lúcles vols introduir?"));
+let num_pel = parseInt(prompt("Quantes pel·lícules vols introduir?"));
 let pelicules = [];
 for (let i = 0; i < num_pel; i++) {
-    pelicules.push(prompt("Pelicula" + i + "?"));
+    pelicules.push(prompt("Pelicula " + (i+1) + "?"));
 }
-let alfabet = confirm("Vols ordenarles alfabèticament?");
-if (alfabet) {
+if (confirm("Vols ordenarles alfabèticament?")) {
     pelicules.sort();
 }
 let usuari = {
@@ -16,5 +15,70 @@ let usuari = {
     pelicules: pelicules
 }
 
-console.log(usuari);
-document.write(usuari);
+function mostrarUsuari () {
+    console.log("Usuari:", usuari);
+    document.getElementById("output").innerHTML =`
+        <p><strong>Nom:</strong> ${usuari.nom}</p>
+        <p><strong>Edat:</strong> ${usuari.edat}</p>
+        <p><strong>Pelicules:</strong> ${usuari.pelicules.join(", ")}</p>
+    `;
+}
+
+
+function afegirPelicula () {
+    let novaPeli = prompt("Introdueix una nova pelicula");
+    if (novaPeli) {
+        usuari.pelicules.push(novaPeli);
+        alert("Pelicula afegida correctament")
+    }
+    mostrarUsuari();
+}
+
+function modificarEdat (){
+    let novaEdat = parseInt(prompt("Introdueix la nova edat:"));
+    if (novaEdat > 0) {
+        usuari.edat = novaEdat;
+        alert("Edat modificada correctament");
+    } else {
+        alert("Edat no valida")
+    }
+    mostrarUsuari();
+}
+
+function eliminarPelicula () {
+    let peliaEliminar = prompt("Quina pelicula vols eliminar?")
+    let index = usuari.pelicules.indexOf(peliaEliminar);
+    if (index !== -1) {
+        usuari.pelicules.splice(index, 1);
+        alert("Pelicula eliminada correctament");
+    } else {
+        alert("Aquesta pel·licula no es troba a la llista")
+    }
+    mostrarUsuari();
+}
+
+function modificarUsuari () {
+    while(true){
+        let opcio = prompt("Què vols fer?\n1.Modificar edat\n2.Afegir pel·licula\n3.Eliminar pel·licula\n4.Exit");
+        switch (opcio) {
+            case "1":
+                modificarEdat();
+                break;
+            case "2":
+                afegirPelicula();
+                break;
+            case "3":
+                eliminarPelicula();
+                break;
+            case "4":
+                alert("Adeu");
+                mostrarUsuari();
+                return;
+            default:
+                alert("Opcio no valida")
+        }
+    }
+}
+modificarUsuari();
+
+
